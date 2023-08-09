4dbf03a341221896bc62d0a4ca01f5ae2d93a3db
Petar Vico <petarvico3@outlook.com>
2023-08-09 15:01:29 -0400

    init navigation and theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { OrderComponent } from './order/order.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "gallery",
+    component: GalleryComponent
+  },
+  {
+    path: "pricing",
+    component: PricingComponent
+  },
+  {
+    path: "order",
+    component: OrderComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { OrderComponent } from './order/order.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    PricingComponent,
+    OrderComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
